275dcee1f73820477520a5e8b2e4ea133df9abf7
Gregg Franklin <greggfranklin@gmail.com>
2018-05-03 09:57:35 -0700

    Update version number

--- local-ssl.php
+++ local-ssl.php
@@ -3,11 +3,10 @@
  * Plugin Name: Local SSL
  * Plugin URL: https://cybercove.io/
  * Description: Allow DesktopServer to use SSL
- * Version: 2.0.7
+ * Version: 2.0.14
  * Author: Joshua Knapp
  * Author URI: http://joshuaknapp.me
  *
  */
  
  //2.x Branch is developed to Work with DS 3.9
-
