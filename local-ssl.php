560301e596918a3a3e546fb1886eadc219903b4f
shadowdao <jknapp85@gmail.com>
2018-05-07 12:05:12 -0700

    Updated to 2.0.15
    Added filter to prevent WordPress from erroring looking at itself over SSL and curl.

--- local-ssl.php
+++ local-ssl.php
@@ -3,11 +3,21 @@
  * Plugin Name: Local SSL
  * Plugin URL: https://cybercove.io/
  * Description: Allow DesktopServer to use SSL
- * Version: 2.0.14
+ * Version: 2.0.15
  * Author: Joshua Knapp
  * Author URI: http://joshuaknapp.me
  *
  */
  
  //2.x Branch is developed to Work with DS 3.9
+ 
+ 
+ 
+ function ds_https_verify() {
+	add_filter( 'https_ssl_verify', '__return_false' );
+}
+
+//add filter after loaded
+add_action('init','ds_https_verify');
+
 
