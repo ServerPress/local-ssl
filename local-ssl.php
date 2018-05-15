bf8b7da179dbd161d0402e6f1660a4381e515182
Stephen J. Carnam <steve@steveorevo.com>
2018-05-15 15:04:20 -0700

    version bump

--- local-ssl.php
+++ local-ssl.php
@@ -3,21 +3,19 @@
  * Plugin Name: Local SSL
  * Plugin URL: https://cybercove.io/
  * Description: Allow DesktopServer to use SSL
- * Version: 2.0.16
+ * Version: 2.0.17
  * Author: Joshua Knapp
  * Author URI: http://joshuaknapp.me
  *
  */
- 
+
  //2.x Branch is developed to Work with DS 3.9
- 
- 
- 
+
+
+
  function ds_https_verify() {
 	add_filter( 'https_ssl_verify', '__return_false' );
 }
 
 //add filter after loaded
 add_action('init','ds_https_verify');
-
-
