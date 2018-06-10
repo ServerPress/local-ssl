cbc7cf9ea2259570dbd40bcb5c7f22d8240044b3
Dave Jesch <dave@davejesch.com>
2018-06-10 11:07:44 -0700

    check for cli before running prepend code; code cleanup; fix potential error with === FALSE instead of == FALSE

--- prepend_1.php
+++ prepend_1.php
@@ -1,14 +1,17 @@
 <?php
 
-//If I'm enabled, the show is SSL Only (with caveats) 
-if ( strpos(strtolower($_SERVER['SCRIPT_NAME']),"xampp") == FALSE) {
+// if running in cli mode, exit now and avoid doing anything
+if ( 'cli' === PHP_SAPI )
+	return;
+
+// If I'm enabled, the show is SSL Only (with caveats)
+if ( FALSE === stripos( $_SERVER['SCRIPT_NAME']), 'xampp' ) ) {
 	if ( !isset( $_SERVER['HTTP_X_ORIGINAL_HOST'] ) && !isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-		if ( $_SERVER['SERVER_NAME'] != 'localhost' && $_SERVER['SERVER_NAME'] != '127.0.0.1' && empty($_SERVER['HTTPS'])) {
+		if ( 'localhost' !== $_SERVER['SERVER_NAME'] && '127.0.0.1' !== $_SERVER['SERVER_NAME'] && empty( $_SERVER['HTTPS'] ) ) {
 			define('FORCE_SSL', TRUE);
-			define('Force_SSL_ADMIN', TRUE);
-			header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+			define('FORCE_SSL_ADMIN', TRUE);
+			header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
 			exit();
 		}
 	}
 }
-?>
