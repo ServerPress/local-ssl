2552e9f91d19c4ccbd902aa61bec946c98bc7f68
Dave Jesch <dave@davejesch.com>
2018-06-11 10:34:33 -0700

    check existence of ['SERVER_NAME'] && $_SERVER['HTTP_HOST'] elements before using them #5

--- prepend_1.php
+++ prepend_1.php
@@ -5,13 +5,15 @@ if ( 'cli' === PHP_SAPI )
 	return;
 
 // If I'm enabled, the show is SSL Only (with caveats)
-if ( FALSE === stripos( $_SERVER['SCRIPT_NAME']), 'xampp' ) ) {
+if ( FALSE === stripos( $_SERVER['SCRIPT_NAME'], 'xampp' ) ) {
 	if ( !isset( $_SERVER['HTTP_X_ORIGINAL_HOST'] ) && !isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-		if ( 'localhost' !== $_SERVER['SERVER_NAME'] && '127.0.0.1' !== $_SERVER['SERVER_NAME'] && empty( $_SERVER['HTTPS'] ) ) {
-			define('FORCE_SSL', TRUE);
-			define('FORCE_SSL_ADMIN', TRUE);
-			header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
-			exit();
+		if ( isset( $_SERVER['SERVER_NAME'] ) && isset( $_SERVER['HTTP_HOST'] ) ) {
+			if ( 'localhost' !== $_SERVER['SERVER_NAME'] && '127.0.0.1' !== $_SERVER['SERVER_NAME'] && empty( $_SERVER['HTTPS'] ) ) {
+				define( 'FORCE_SSL', TRUE );
+				define( 'FORCE_SSL_ADMIN', TRUE );
+				header( 'Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
+				exit();
+			}
 		}
 	}
 }
