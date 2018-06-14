bd119b6dad754eeda4be47164f7cea59a10626e7
Dave Jesch <davejesch@gmail.com>
2018-06-14 16:27:28 -0700

    change msg to indicate Local SSL plugin instead of Database Archive; add slug prefix to function name; use PHP_SAPI constant instead of method call

--- local-ssl.php
+++ local-ssl.php
@@ -5,6 +5,7 @@
  * Description: Allow DesktopServer to use SSL
  * Version: 2.0.20
  * Author: Joshua Knapp
+ * Text Domain: local-ssl
  * Author URI: http://joshuaknapp.me
  *
  */
@@ -12,39 +13,40 @@
  //2.x Branch is developed to Work with DS 3.9
 
  //stop if running in cli
-if (php_sapi_name() == "cli") {
+if ( 'cli' === PHP_SAPI ) {
     // In cli-mode
 	return;
 }
 
 //detect if we are in the ds-plugins folder
-if ( strpos(__DIR__,"ds-plugins") === FALSE ) {
+if ( FALSE === strpos( __DIR__, 'ds-plugins' ) ) {
 	// detect if not in the ds-plugins folder
 	if ( is_admin() ) {
-		add_action( 'admin_notices', 'install_message' );
+		add_action( 'admin_notices', 'local_ssl_install_message' );
 		return;		// do not initialize the rest of the plugin
 	}
 }
 
 //Throw Error Message for WordPress if LocalSSL is not where it should be
- function install_message()
-	{
-		if ( 'Darwin' === PHP_OS )
-			$correct_dir = '/Applications/XAMPP/ds-plugins/';		// mac directory
-		else
-			$correct_dir = 'C:\\xampplite\\ds-plugins\\';			// Windows directory
+function local_ssl_install_message()
+{
+	if ( 'Darwin' === PHP_OS )
+		$correct_dir = '/Applications/XAMPP/ds-plugins/';		// mac directory
+	else
+		$correct_dir = 'C:\\xampplite\\ds-plugins\\';			// Windows directory
 
-		echo '<div class="notice notice-error">',
-			'<p>',
-			sprintf( __('The Database Archive plugin needs to be installed in Desktop Server\'s ds-plugins directory.<br/>Please install in %1$slocal-ssl', 'database-archive' ),
-				$correct_dir),
-			'</p>',
-			'</div>';
+	echo '<div class="notice notice-error">',
+		'<p>',
+		sprintf( __('The Local SSL plugin needs to be installed in Desktop Server\'s ds-plugins directory.<br/>Please install in %1$slocal-ssl', 'local-ssl' ),
+			$correct_dir),
+		'</p>',
+		'</div>';
 }
 
 
 //Prevent WordPress from trying to validate the SSL 
- function ds_https_verify() {
+function ds_https_verify()
+{
 	add_filter( 'https_ssl_verify', '__return_false' );
 }
 
